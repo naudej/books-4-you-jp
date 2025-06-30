704117959d39db6cd9e0fd97b53d86afe05fa1f6
Juan-Paul Naude <Juan-Paul.Naude@extcom.com>
2025-06-30 14:33:25 +0100

    feat(add-form): Added validation for ISBN Type

--- src/form/AddBook.tsx
+++ src/form/AddBook.tsx
@@ -22,7 +22,7 @@ import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { IMaskInput } from 'react-imask';
 import { ISBN_TYPES } from '../data/types.ts';
-
+import isISBN from 'validator/es/lib/isISBN';
 const isbn10Mask = '0-0000-0000-0';
 const isbn13Mask = '000-0-000-00000-0';
 
@@ -34,7 +34,17 @@ const validationSchema = yup.object({
     .required('Was the book written by a ghost?'),
   publishedDate: yup.date().required('Published date is required').typeError('Invalid date'),
   isbnType: yup.string().oneOf(Object.values(ISBN_TYPES)).required('Please select ISBN type'),
-  isbn: yup.string().required('Librarians would be mad without this, please dont make one cry'),
+  isbn: yup
+    .string()
+    .required('Librarians would be mad without this, please dont make one cry')
+    .test('is-valid-isbn', 'Invalid ISBN for selected type', function (value) {
+      const { isbnType } = this.parent;
+      if (!value || !isbnType) {
+        return false;
+      }
+      const version = isbnType === ISBN_TYPES.ISBN_10 ? '10' : '13';
+      return isISBN(value, version);
+    }),
 });
 
 type FormFields = InferType<typeof validationSchema>;
