8065d489fbaa25268aa06d95cf1fe3a604e6a053
Juan-Paul Naude <Juan-Paul.Naude@extcom.com>
2025-07-03 20:01:26 +0100

    update(app): Add an ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Outlet } from 'react-router';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router';
 import { createTheme } from '@mui/material/styles';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
 const theme = createTheme({
   cssVariables: {
@@ -19,31 +20,21 @@ const theme = createTheme({
   },
 });
 
-// const NAVIGATION: Navigation = [
-//     {
-//         title: 'Catalogue',
-//     },
-//     {
-//         title: 'BookDetails',
-//         pattern: 'book{/:employeeId}*',
-//     },
-// ];
-
-//@TODO CHeck this
 const BRANDING = {
   title: 'Books 4 You!',
   homeUrl: '/',
+  logo: <AutoStoriesIcon color="primary" fontSize="large" />,
 };
 
 //@TODO Add a pre-push for simple-git-hooks that run the NPM tests
-//@TODO Page Error boundary see App.tsx (libraries/client-core/src/components/layout/Page.tsx)
 //@TODO Add suspense fallback with 404 page cannot be found or something went wrong
 //@TODO NEXT STEP: LOCALIZATION
 //@TODO: Super nice to have a detector if the user is disconnected from the internet
-export default function App() {
+const App: React.FC = () => {
   return (
     <ReactRouterAppProvider theme={theme} branding={BRANDING}>
       <Outlet />
     </ReactRouterAppProvider>
   );
-}
+};
+export default App;
--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,9 +1,48 @@
 import * as React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import Paper from '@mui/material/Paper';
 import { SnackbarProvider } from './context/SnackBarContext.tsx';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import { Button, Grid, Stack } from '@mui/material';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import Typography from '@mui/material/Typography';
+import type { ErrorInfo } from 'react';
+
+const ErrorFallback: React.FC<FallbackProps> = ({ resetErrorBoundary }) => {
+  const navigate = useNavigate();
+  const retry = () => {
+    navigate('/');
+    resetErrorBoundary();
+  };
+
+  return (
+    <Grid
+      container={true}
+      sx={{ height: '100%', padding: '100px' }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack spacing={2} alignItems="center">
+        <WarningAmberIcon color="error" sx={{ fontSize: 60 }} />
+        <Typography variant="h4">Omg why is this happening!</Typography>
+        <Typography variant="body1" color="textSecondary" textAlign="center">
+          I hope the rest of the demo goes better, maybe I can win them over with my charm or at
+          least being funny. Maybe being funny. Its a long shot.
+        </Typography>
+        <Button variant="contained" onClick={retry}>
+          Retry
+        </Button>
+      </Stack>
+    </Grid>
+  );
+};
+
+const logError = (error: Error, info: ErrorInfo) => {
+  console.error('I would usually log this to Sentry but for now, Uncaught error:', error);
+  console.error('Component Stack:', info.componentStack);
+};
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,7 +53,9 @@ const Dashboard: React.FC = () => {
             elevation={3}
             sx={{ width: '100%', height: '100%', padding: '25px', borderRadius: '10px' }}
           >
-            <Outlet />
+            <ErrorBoundary FallbackComponent={ErrorFallback} onError={logError}>
+              <Outlet />
+            </ErrorBoundary>
           </Paper>
         </PageContainer>
       </SnackbarProvider>
